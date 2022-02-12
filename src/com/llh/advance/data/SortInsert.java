1b49a3aca496939466eb379d879230a0e19c0f2b
lviter <lviter@163.com>
2022-02-12 15:24:45 +0800

    新增springCloud相关

--- src/com/llh/advance/data/SortInsert.java
+++ src/com/llh/advance/data/SortInsert.java
@@ -1,4 +1,36 @@
 package com.llh.advance.data;
-
+/**
+ * @author: lviter
+ * @desc:
+ * @date:2022/1/25
+ */
 public class SortInsert {
+
+    public static void main(String[] args) {
+        // 需要排序的数组
+        int arr[] = { 49, 20, 36, 51, 18, 94, 61, 31, 50 };
+        int res[] = sortArrays(arr);
+        if(res.length > 0){
+            for (int i = 0; i < res.length; i++) {
+                System.out.println(arr[i] + "\t");
+            }
+        }
+    }
+
+    private static int[] sortArrays(int[] arr) {
+
+        int res[] = arr;
+
+        int temp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int j = i - 1;
+            temp = arr[i];
+            for (;j >= 0;j--){
+
+            }
+        }
+
+
+        return new int[5];
+    }
 }
